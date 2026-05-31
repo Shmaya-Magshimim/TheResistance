a7a387a386a07e9ff2815318d01eeb30fd8dd614
Shmaya Peskin <shmaya.peskin@xmcyber.com>
2026-05-31 17:26:14 +0300

    ADD : Buisness Layer

---
+++ Zoo/src/BuisnessLayer.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BuisnessLayer {
+    private Animal[] animals;
+    private Queue<Animal> vetList = new LinkedList<>() ;
+    
+    
+}
